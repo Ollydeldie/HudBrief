444f1f57aff8e85a86b2d084211248994e3be713
Ollydeldie <98750663+Ollydeldie@users.noreply.github.com>
2023-03-09 11:49:01 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,2 +1,22 @@
 <?php
 ?>
+<html>
+  <head>
+    <body class="login">
+      <form class="login_form action="login.php" method="POST">
+          <label for="username">Username:</label><br/>
+          <input type="text" name="username" required/><br/><br/>
+          <label for="password">Password:</label><br/>
+          <input type="password" name="password" required/><br/><br/>
+          <?php
+          if (!empty($error)) {
+              ?>
+              <p id="login_form_error"><?php echo $error ?></p><br>
+              <?php 
+          }
+          ?>
+          <input type="submit" value="Login"/>
+      </form>
+    </body>
+  </head>
+</html>
