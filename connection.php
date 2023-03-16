c8ec77c7183d04cd10e563665b1685ed16387736
Ollydeldie <98750663+Ollydeldie@users.noreply.github.com>
2023-03-16 12:18:43 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,10 @@
 <?php
 $servername = "selene.hud.ac.uk"
+$username = "hudbrief"; 
+$password = "HB16mar23hb";
+$dname = "hudbreif";
   
-$conn = new mysqli($servername);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if($conn->connect_error){
   die("Connection failed: ". $conn->connect_error);
