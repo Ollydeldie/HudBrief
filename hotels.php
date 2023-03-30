81457e3ec27bc4193ec566b0eed8982318fc7242
Ollydeldie <98750663+Ollydeldie@users.noreply.github.com>
2023-03-30 12:07:50 +0100

    Update and rename venues.php to hotels.php

---
+++ hotels.php
@@ -0,0 +1,25 @@
+<?php
+require_once('connection.php');
+?>
+<html>
+  <title>Hotels</title>
+  <link href="styles.css" rel="stylesheet" type="text/css" media="screen and (min-width: 500px)">
+  <body>
+    <nav id="navi">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="restaurants.php">Restaurants</a></li>
+          <li><a href="nightouts.html">Nights Out</a></li>
+          <li><a class="active" href="hotels.php">Hotels</a></li>
+          <li><a href="events.html">Upcoming Events</a></li>
+          <li><a href="attractions.php">Tourist Attractions</a></li>
+          <li><a href="transport.html">Public Transport</a></li>
+        </ul>
+      </nav>
+    
+    <div class="intro">
+        <h2> Upcoming Events</h2>
+        <p1> Here are the best Hotels the town has to offer: </p1>
+      </div>
+  </body>
+  </html>
